e0fb9533797d58250be6494186b6f9d90dbaaeee
korumilli varun sai nadh <118454392+varunsainadh-116@users.noreply.github.com>
2025-05-24 10:43:45 +0530

    Update CalendarHeader.js

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -21,7 +21,7 @@ export default function CalendarHeader() {
     <header className="px-4 py-2 flex items-center">
       <img src={logo} alt="calendar" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 fond-bold">
-        Calendar
+        Event Calendar
       </h1>
       <button
         onClick={handleReset}
